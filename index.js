0293569b3c175612ec68fd175851408d0646541a
Malcolm Keyes <3.malcolmk@gmail.com>
2022-07-11 11:22:34 -0400

    commit tree f9a554ff9d183c92a869bb64baadac8c444d16e5

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+let main = document.querySelector("#main");
+main.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Malcolm is the champion!";
+document.body.appendChild(newHeader);
